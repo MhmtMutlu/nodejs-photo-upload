41da8ef90831a79d0f944f67056ea2dd8240d21c
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-01-23 15:21:31 +0300

    Feat-UI: index.html page was used with express static

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+//! Middleware
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Merhaba');
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 app.listen(port, () => {
