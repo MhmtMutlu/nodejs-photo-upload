c1d0f937514df6fbba5ce2e75c0ee9879811082d
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-01-23 16:18:47 +0300

    Feat-UI: Ejs was used for pages

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
+//! Template Engine
+app.set('view engine', 'ejs');
+
 //! Middleware
 app.use(express.static('public'));
 
+//! Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
 
 app.listen(port, () => {
