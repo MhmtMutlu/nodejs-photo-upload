37d8196d301042eb9fe11f91bf2ee3dc3d47dc0c
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-01-27 00:19:32 +0300

    Feat-UI: Photo adding operations were completed

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const Photo = require('./models/Photo');
 
 const app = express();
@@ -20,11 +22,12 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 //! Routes
 app.get('/', async (req, res) => {
   // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-createdDate');
   res.render('index', {
     photos,
   });
@@ -43,8 +46,22 @@ app.get('/photos/:id', async (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 app.listen(port, () => {
